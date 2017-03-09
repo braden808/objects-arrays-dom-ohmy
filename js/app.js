b120b5c9ad584f7c977d75976d2e25329a1499e7
jmt75200 <joelle@pcgamerzhawaii.com>
2017-03-09 08:18:18 -1000

    hip hop hurray

---
+++ js/app.js
@@ -0,0 +1,12 @@
+/*
+* Character Creation
+*
+* Create a variable with the name character
+* Assign the value of character an array of objects
+* Create two objects with data. Use the list of options to set the values
+* gender options: boy, girl
+* shirt options: red, blue, purple
+* hobby options: baseball, beach volleyball, singing
+*
+*/
+
